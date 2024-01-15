00f3ba90cb245085804c2ba097822be059944e4d
Jonathan Zentio <j.zentio@gmail.com>
2024-01-15 15:39:49 +0100

    Changed theme to Catppuccin

--- init.lua
+++ init.lua
@@ -189,12 +189,12 @@ require('lazy').setup({
     },
   },
 
-  {
-    -- Theme inspired by Atom
-    'navarasu/onedark.nvim',
+   {
+    'catppuccin/nvim',
+    name = 'catppuccin',
     priority = 1000,
     config = function()
-      vim.cmd.colorscheme 'onedark'
+      vim.cmd.colorscheme 'catppuccin'
     end,
   },
 
